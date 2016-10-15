dba2a0aede48e162619f843391136245d88e4c82
Matthew Epstein <mepstein9@yahoo.com>
2016-10-15 02:01:17 -0400

    beat and speech run simultaneously

--- beepTest.py
+++ beepTest.py
@@ -2,10 +2,15 @@ import os
 import math
 import pyaudio
 from time import sleep
+import thread
 import threading
 
-def beep(num):
-	print("in beep")
+
+def speak():
+	os.system("say 'Now this is a story all about how My life got flipped-turned upside down And I'd like to take a minute Just sit right there I'll tell you how I became the prince of a town called Bel-Air'")
+
+
+def beep():
 	#sudo apt-get install python-pyaudio
 	PyAudio = pyaudio.PyAudio
 
@@ -24,10 +29,7 @@ def beep(num):
 		WAVEDATA = WAVEDATA+chr(int(math.sin(x/((BITRATE/FREQUENCY)/math.pi))*127+128))
 
 
-	counter = 0
-
-	while(counter < num):
-		print(counter)
+	while( speak_t.is_alive() ):
 	 	p = PyAudio()
 		stream = p.open(format = p.get_format_from_width(1), 
 		                channels = 1, 
@@ -37,23 +39,18 @@ def beep(num):
 		stream.stop_stream()
 		stream.close()
 		p.terminate()
-	 	counter = counter+1
 	 	sleep(1)
 
-def speak(num):
-	sleep(1)
-	os.system("say 'Now this is a story all about how My life got flipped-turned upside down And I would d like to take a minuteJust sit right there I will tell you how I became the prince of a town called Bel-Air'")
+beep_t = threading.Thread(target=beep, args=())
+speak_t = threading.Thread(target=speak, args=())
 
 def init_threads():
-	num = 3
 	try:
-		beep_t = threading.Thread(target=beep, args=(num,))
-		speak_t = threading.Thread(target=speak, args=(num,))
-		beep_t.start()
 		speak_t.start()
+		beep_t.start()
 		# wait for threads
-		beep_t.join()
 		speak_t.join()
+		beep_t.join()
 
 	except:
 		print("Error")
